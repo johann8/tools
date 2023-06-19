8a31b3149697b7bb7d124c88624c6dce950a6221
johann8 <jo.hahn@gmx.de>
2023-06-19 15:51:24 +0200

    small fixes

--- docker_microservice_update.sh
+++ docker_microservice_update.sh
@@ -8,7 +8,10 @@ set -o errexit
 ### === Set Variables ===
 #
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+BASENAME="${0##*/}"
+SCRIPT_VERSION="0.1.0"                  # Set script version
 
+# CUSTOM - define colors
 esc=""
 bluef="${esc}[34m"; redf="${esc}[31m"; yellowf="${esc}[33m"; greenf="${esc}[32m"; cyanf="${esc}[36m"; pinkf="${esc}[35m"
 blackf="${esc}[30m"; whitef="${esc}[37m"; xxxf="${esc}[1;32m";
@@ -19,6 +22,10 @@ reset="${esc}[0m"
 ### === Functions ===
 #
 # Function print kopf
+print_basename() { 
+   echo -e "${pinkf}${BASENAME}:${reset} $1"
+}
+
 print_kopf() {
    echo "${greenf}======================================================${reset}"
 }
@@ -41,18 +48,23 @@ print_kopf_short() {
 #
 ### ============= Main script ============
 #
+# Print script version
+print_basename "Script version is: ${cyanf}\"${SCRIPT_VERSION}\"${reset}"
+print_basename "${greenf}+-----------------------------------------------------------------------------------+${reset}"
+print_basename "${greenf}|${reset} Start update on host: ${cyanf}\"$(hostname -f)\"${reset} at ${cyanf}\"${TIMESTAMP}\"${reset} ${greenf}|${reset}"
+print_basename "${greenf}+-----------------------------------------------------------------------------------+${reset}\n"
 
 # Put all running containers in array
 ar_lc=($(docker-compose ps |awk '{print $1}' |awk '(NR>1)'))
 
-print_kopf
-echo "There are/is \"${cyanf}${#ar_lc[*]}${reset}\" docker container(s):${cyanf}  \"$(echo ${ar_lc[*]})\""${reset}
-print_kopf
+print_basename ${greenf}======================================================${reset}
+print_basename "There are/is \"${cyanf}${#ar_lc[*]}${reset}\" docker container(s):${cyanf}  \"$(echo ${ar_lc[*]})\""${reset}
+print_basename ${greenf}======================================================${reset}
 for i in ${ar_lc[*]}; do
    STATUS_CONTAINER=$(docker inspect -f '{{.State.Status}}' ${i})
-   printf "%-20s %-27s %1s %-5s\n" "Docker Container:" "${cyanf} $i${reset}" "-" "${bluef}${STATUS_CONTAINER}${reset}"
+   echo -e "${pinkf}${BASENAME}:${reset}; printf "%-20s %-27s %1s %-5s\n" "Docker Container:" "${cyanf} $i${reset}" "-" "${bluef}${STATUS_CONTAINER}${reset}"
 done
-print_foot
+print_basename "${greenf}------------------------------------------------------${reset}"
 
 # Check if there is/are an update of docker image; pull docker image
 for i in ${ar_lc[*]}; do
@@ -64,39 +76,39 @@ for i in ${ar_lc[*]}; do
    CONTAINER_IMAGE="$(docker inspect --format "{{.Config.Image}}" --type container ${i})"
 
    echo ""
-   print_kopf_long
-   echo "  Updating Docker Image ${cyanf}\"${i}\"${reset} am ${TIMESTAMP}"
-   print_kopf_long
+#   print_kopf_long
+#   print_basename "  Updating Docker Image ${cyanf}\"${i}\"${reset} am ${TIMESTAMP}"
+#   print_kopf_long
    docker pull ${CONTAINER_IMAGE} 2> /dev/null
    LATEST_IMAGE="$(docker inspect --format "{{.Id}}" --type image "${CONTAINER_IMAGE}")"
 
-   echo "${greenf}Container name:${cyanf} ${i}${reset}"
-   echo "${greenf}Container ${cyanf}${i} ${greenf}- running image hash:${reset} ${bluef}${RUNNING_IMAGE}${reset}"
-   echo "${greenf}Container ${cyanf}${i} ${greenf}- pulled image hash:${reset}  ${redf}${LATEST_IMAGE}${reset}"
+   print_basename "${greenf}Container name:${cyanf} ${i}${reset}"
+   print_basename "${greenf}Container ${cyanf}${i} ${greenf}- running image hash:${reset} ${bluef}${RUNNING_IMAGE}${reset}"
+   print_basename "${greenf}Container ${cyanf}${i} ${greenf}- pulled image hash:${reset}  ${redf}${LATEST_IMAGE}${reset}"
 
    # Update / Exit
    if ! [ ${RUNNING_IMAGE} = ${LATEST_IMAGE} ]; then
-      echo "${redf}There is an update of docker image: ${cyanf}\"${i}\"${reset}"
+      print_basename "${redf}There is an update of docker image: ${cyanf}\"${i}\"${reset}"
       RES=true
    else
-      echo "${redf}There is no update of docker image: ${cyanf}\"${i}\"${reset}"
+      print_basename "${redf}There is no update of docker image: ${cyanf}\"${i}\"${reset}"
    fi
 done
 
 # Restart and delete microservices
 if [[ "${RES}" = true ]]; then
    echo ""
-   print_kopf_short
-   echo "${greenf}  There is/are an update of docker image(s)!${reset}"
-   print_kopf_short
-   echo "${greenf}  Microservice is restarting... ${reset}"
+   print_basename "${greenf}==============================================${reset}"
+   print_basename "${greenf}  There is/are an update of docker image(s)!${reset}"
+   print_basename "${greenf}==============================================${reset}"
+   print_basename "${greenf}  Microservice is restarting... ${reset}"
    docker-compose down && docker-compose up -d
 
-   echo "${greenf} Deleting dangling images... ${reset}"
+   print_basename "${greenf} Deleting dangling images... ${reset}"
    docker rmi $(docker images -f "dangling=true" -q --no-trunc)
 else
    echo ""
    print_kopf_short
-   echo "${redf}  There is/are no update of docker image(s):${cyanf}  \"$(echo ${ar_lc[*]})\""${reset}
+   print_basename "${redf}  There is/are no update of docker image(s):${cyanf}  \"$(echo ${ar_lc[*]})\""${reset}
    print_kopf_short
 fi
