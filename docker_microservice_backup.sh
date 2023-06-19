f34b6cfc21404a7a6050dbdde87c198bec678273
johann8 <jo.hahn@gmx.de>
2023-06-19 17:50:36 +0200

    small fixes

--- docker_microservice_backup.sh
+++ docker_microservice_backup.sh
@@ -7,19 +7,18 @@ set -o errexit
 #
 ### === Set Variables ===
 #
+
+# CUSTOM - script
+SCRIPT_NAME='backup_docker_container'
+BASENAME="${0##*/}"
+SCRIPT_VERSION="0.1.1"
+_HOST=$(echo $(hostname) | cut -d"." -f1)
+
+# CUSTOM vars - define colors
 esc=""
 bluef="${esc}[34m"; redf="${esc}[31m"; yellowf="${esc}[33m"; greenf="${esc}[32m"; cyanf="${esc}[36m"; pinkf="${esc}[35m"; xxxf="${esc}[1;32m"
 boldon="${esc}[1m"; boldoff="${esc}[22m"
 reset="${esc}[0m"
-
-# CUSTOM - script
-SCRIPT_NAME='backup_docker_container'
-_HOST=$(echo $(hostname) | cut -d"." -f1)
-basename="${0##*/}"
-
-# Print script name
-print_basename() { echo "${pinkf}${basename}:${reset} $1"; }
-SCRIPT_VERSION="0.1.0"
 
 # CUSTOM - Variables
 TIMESTAMP=$(date '+%Y%m%d_%H%M%S')
@@ -36,81 +35,92 @@ ALLCONTAINER=$(docker ps --format '{{.Names}}')
 COMPOSE_PROJECTS_PATH=$(for i in $ALLCONTAINER; do docker inspect --format '{{ index .Config.Labels "com.docker.compose.project.working_dir"}}' $i; done | sort -u)
 COMPOSE_PROJECTS_NAME=($(docker inspect $(docker ps -q) --format '{{ index .Config.Labels "com.docker.compose.project"}}' | uniq))
 
-# CUSTOM - Backup-Files.
+# CUSTOM - Backup files
 BACKUPDIR="/var/backup/${_HOST}/compose_projects"
 FILE_BACKUP=backup-$(echo ${TIMESTAMP}).tzst
 FILE_DELETE='*.tzst'
 DAYS_NUMBER=30
 
 #
+### === Functions ===
+#
+
+# Function print script basename
+print_basename() {
+   echo -e -n "${pinkf}${BASENAME}:${reset} $1"
+}
+
+#
 ### ============= Main script ============
 #
 
-echo -e "+-----------------------------------------------------------------+"
-echo -e "|  ${TIMESTAMP1} Start Backup for Docker Compose Projects!  |"
-echo -e "+-----------------------------------------------------------------+\n"
+#echo -e "|  ${TIMESTAMP1} Start Backup for Docker Compose Projects!  |"
+
+print_basename "Script version is: ${cyanf}\"${SCRIPT_VERSION}\"${reset}"
+print_basename "${greenf}+-----------------------------------------------------------------------------------+${reset}"
+print_basename "${greenf}|${reset} Start backup on host: ${cyanf}\"$(hostname -f)\"${reset} at ${cyanf}\"${TIMESTAMP}\"${reset} ${greenf}|${reset}"
+print_basename "${greenf}+-----------------------------------------------------------------------------------+${reset}\n"
 
 # check path FILE_BACKUP
 if [ ! -d ${BACKUPDIR} ]; then
-   echo -n "Creating  "
+   print_basename "Creating backup dir..."
    mkdir -p ${BACKUPDIR}
 fi
 
 # Check if command (file) NOT exist OR IS empty.
 if [ ! -s "${ZSTD_COMMAND}" ]; then
-   echo -e "Check if command '${ZSTD_COMMAND}' was found..............[FAILED]"
+   print_basename "Check if command '${ZSTD_COMMAND}' was found..............[FAILED]"
    exit 11
 else
-   echo -e "Check if command '${ZSTD_COMMAND}' was found.............[  OK  ]"
+   print_basename "Check if command '${ZSTD_COMMAND}' was found.............[  OK  ]"
 fi
 
 # Check if command (file) NOT exist OR IS empty.
 if [ ! -s "${DOCKER_COMPOSE_COMMAND}" ]; then
-   echo -e "Check if command '${DOCKER_COMPOSE_COMMAND}' was found...[FAILED]"
+   print_basename "Check if command '${DOCKER_COMPOSE_COMMAND}' was found...[FAILED]"
    exit 12
 else
-   echo -e "Check if command '${DOCKER_COMPOSE_COMMAND}' was found...[  OK  ]"
+   print_basename "Check if command '${DOCKER_COMPOSE_COMMAND}' was found...[  OK  ]"
 fi
 
 # Check if command (file) NOT exist OR IS empty.
 if [ ! -s "${TAR_COMMAND}" ]; then
-   echo -e "Check if command '${TAR_COMMAND}' was found..............[FAILED]"
+   print_basename "Check if command '${TAR_COMMAND}' was found..............[FAILED]"
    exit 13
 else
-   echo -e "Check if command '${TAR_COMMAND}' was found..............[  OK  ]"
+   print_basename "Check if command '${TAR_COMMAND}' was found..............[  OK  ]"
 fi
 
 ### Run backup
 #ar_lc=($(docker-compose ps |awk '{print $1}' |awk '(NR>1)'))
-echo -e "\n==========================================="
-echo " There are \"${#COMPOSE_PROJECTS_NAME[*]}\" Docker Composer Project(s): ${COMPOSE_PROJECTS_NAME[*]}"
-echo -e "===========================================\n"
+print_basename "\n==========================================="
+print_basename " There are \"${#COMPOSE_PROJECTS_NAME[*]}\" Docker Composer Project(s): ${COMPOSE_PROJECTS_NAME[*]}"
+print_basename -e "===========================================\n"
 
 # Backup all Docker Composer Project(s)
 for i in ${COMPOSE_PROJECTS_PATH}; do
     cd ${i}
 
-    echo "============ ${i##*/} ============"
-    echo "Project working dir: ${i}"
+    print_basename "============ ${i##*/} ============"
+    print_basename "Project working dir: ${i}"
 
     #
     ar_lc=($(docker-compose ps |awk '{print $1}' |awk '(NR>1)'))
-    echo "There are \"${#ar_lc[*]}\" container(s) in this project: ${ar_lc[*]}"
+    print_basename "There are \"${#ar_lc[*]}\" container(s) in this project: ${ar_lc[*]}"
 
     # Stop all containers, make backup and start all containers
-    echo -e "\n${TIMESTAMP2} Docker Compose Project \"${i##*/}\" is backed up... "
-    echo -e "All docker containers are stopped... "
+    print_basename "\n${TIMESTAMP2} Docker Compose Project \"${i##*/}\" is backed up... "
+    print_basename "All docker containers are stopped... "
     ${DOCKER_COMPOSE_COMMAND} down
-    #echo "[ DONE ]"
 
-    echo -e "\nDoing compose project backup... "
+    print_basename "\nDoing compose project backup... "
     ${TAR_COMMAND} -I 'zstd -15 -T0' -cvf ${BACKUPDIR}/${i##*/}-${FILE_BACKUP} ${TAR_OPTIONS} ${TAR_EXCLUDE} . > /dev/null 2>&1
 
-    echo -e "All docker containers are started... \n"
+    print_basename "All docker containers are started... \n"
     ${DOCKER_COMPOSE_COMMAND} up -d
     #echo "[ DONE ]"
 
-    echo -e "\n${TIMESTAMP2} Docker Compose Project \"${i##*/}\" is successfully backed up\n\n"
+    print_basename "\n${TIMESTAMP2} Docker Compose Project \"${i##*/}\" is successfully backed up\n\n"
 
 #    #
 #    for n in ${ar_lc[*]}; do
@@ -122,9 +132,9 @@ for i in ${COMPOSE_PROJECTS_PATH}; do
 #    done
 done
 
-echo -e "+-----------------------------------------------------------------+"
-echo -e "|   ${TIMESTAMP1} Backup for Compose Projects completed!    |"
-echo -e "+-----------------------------------------------------------------+"
+print_basename "+-----------------------------------------------------------------+"
+print_basename "|   ${TIMESTAMP1} Backup for Compose Projects completed!    |"
+print_basename "+-----------------------------------------------------------------+"
 
 # echo -e "\n$TIMESTAMP Backup for Compose Projects completed\n"
 
