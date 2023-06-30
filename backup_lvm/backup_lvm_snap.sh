f00df84119621dbbe4b4c65d63caf0bc019877f5
johann8 <jo.hahn@gmx.de>
2023-06-30 11:52:11 +0200

    added function sendmail; changed SCRIPT_VERSION

--- backup_lvm/backup_lvm_snap.sh
+++ backup_lvm/backup_lvm_snap.sh
@@ -12,11 +12,11 @@
 #               will be send by e-mail.                                      #
 #                                                                            #
 # Created     : 02.08.2022                                                   #
-# Last update : 03.08.2022                                                   #
-# Version     : 1.01                                                         #
+# Last update : 30.06.2023                                                   #
+# Version     : 0.1.5                                                        #
 #                                                                            #
-# Author      : Johann Hahn, <j.hahn@wassermannkabeltechnik.de>              #
-# DokuWiki    : https://docu.int.wassermanngruppe.de                         #
+# Author      : Johann Hahn, <j.hahn@wassermann*****technik.de>              #
+# DokuWiki    : https://docu.***.wassermanngruppe.de                         #
 # Homepage    : https://wassermanngruppe.de                                  #
 # GitHub      : https://github.com/johann8/tools                             #
 # Download    : https://raw.githubusercontent.com/johann8/tools/master/ \    #
@@ -34,19 +34,6 @@
 ##############################################################################
 
 ##############################################################################
-#                                H I S T O R Y                               #
-##############################################################################
-# -------------------------------------------------------------------------- #
-# Version     : 1.01                                                         #
-# Description : Changed var area                                             #
-# -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #
-# Version     : x.xx                                                         #
-# Description : <Description>                                                #
-# -------------------------------------------------------------------------- #
-##############################################################################
-
-##############################################################################
 # >>> Please edit following lines for personal settings and custom usages. ! #
 ##############################################################################
 
@@ -59,7 +46,9 @@ SNAPSIZE=5G                                                  # space to allocate
 # CUSTOM - script
 SCRIPT_NAME="backupLVS.sh"                                   # LVS - logical volume snapshot
 BASENAME=${SCRIPT_NAME}
-SCRIPT_VERSION="0.1.4"
+SCRIPT_VERSION="0.1.5"
+
+# CUSTOM - vars
 #BASENAME="${0##*/}"
 SCRIPTDIR="${0%/*}"
 BACKUPDIR="/mnt/NAS_BareOS/docker/$(hostname -s)/lvm-snapshot/$(date "+%Y-%m-%d")"  # where to put the backup
@@ -69,8 +58,27 @@ BACKUPNAME="${ORIGVOL}_${TIMESTAMP}.tgz"                           # name of the
 TAR_EXCLUDE_VAR="--exclude-from=${SCRIPTDIR}/tar_exclude_var.txt"  # Files to be excluded from tar archive
 MOUNTDIR="/mnt/lvm_snap"
 SEARCHDIR="${BACKUPDIR%/*}"
-LOGFILE="/var/log/container_backup.log"
-BACKUP_DAYS=6                                                      # Number of backup files
+
+
+# CUSTOM - logs
+FILE_LAST_LOG='/tmp/'${SCRIPT_NAME}'.log'
+FILE_MAIL='/tmp/'${SCRIPT_NAME}'.mail'
+
+# CUSTOM - Send mail
+MAIL_STATUS='Y'                                 # Send Status-Mail [Y|N]
+PROG_SENDMAIL='/sbin/sendmail'
+VAR_HOSTNAME=$(uname -n)
+VAR_SENDER='root@'${VAR_HOSTNAME}
+VAR_EMAILDATE=$(date '+%a, %d %b %Y %H:%M:%S (%Z)')
+
+# CUSTOM - Mail-Recipient.
+MAIL_RECIPIENT='you@example.com'
+
+# CUSTOM - Days number of stored backups
+BACKUP_DAYS=6
+
+#FILE_LAST_LOG="/var/log/container_backup.log"
+#BACKUP_DAYS=6                                                      # Number of backup files
 
 ##############################################################################
 # >>> Normaly there is no need to change anything below this comment line. ! #
@@ -85,82 +93,108 @@ print_basename() {
    echo -e "${BASENAME}: $1"
 }
 
+# Function: send mail
+function sendmail() {
+     case "$1" in
+     'STATUS')
+               MAIL_SUBJECT='Status execution '${SCRIPT_NAME}' script.'
+              ;;
+            *)
+               MAIL_SUBJECT='ERROR while execution '${SCRIPT_NAME}' script !!!'
+               ;;
+     esac
+
+cat <<EOF >$FILE_MAIL
+Subject: $MAIL_SUBJECT
+Date: $VAR_EMAILDATE
+From: $VAR_SENDER
+To: $MAIL_RECIPIENT
+EOF
+
+# sed: Remove color and move sequences
+echo -e "\n" >> $FILE_MAIL
+cat $FILE_LAST_LOG  >> $FILE_MAIL
+${PROG_SENDMAIL} -f ${VAR_SENDER} -t ${MAIL_RECIPIENT} < ${FILE_MAIL}
+rm -f ${FILE_MAIL}
+}
+
+
 #
 ### ============= Main script ============
 #
 
-echo -e "\n"
-print_basename "Script version is: \"${SCRIPT_VERSION}\"" | tee -a ${LOGFILE}
-print_basename "Datum: $(date "+%Y-%m-%d")" | tee -a ${LOGFILE}
-print_basename "============================" | tee -a ${LOGFILE}
-print_basename " Run backup of LV snapshot" | tee -a ${LOGFILE}
-print_basename "============================" | tee -a ${LOGFILE}
-print_basename "Started on \"$(hostname -f)\" at \"${_DATUM}\"" | tee -a ${LOGFILE}
-echo " " | tee -a ${LOGFILE}
+echo -e "\n" 2>&1 | tee ${FILE_LAST_LOG}
+print_basename "Script version is: \"${SCRIPT_VERSION}\"" 2>&1 | tee ${FILE_LAST_LOG}
+print_basename "Datum: $(date "+%Y-%m-%d")" 2>&1 | tee -a ${FILE_LAST_LOG}
+print_basename "============================" 2>&1 | tee -a ${FILE_LAST_LOG}
+print_basename " Run backup of LV snapshot" 2>&1 | tee -a ${FILE_LAST_LOG}
+print_basename "============================" 2>&1 | tee -a ${FILE_LAST_LOG}
+print_basename "Started on \"$(hostname -f)\" at \"${_DATUM}\"" 2>&1 | tee -a ${FILE_LAST_LOG}
+echo " " 2>&1 | tee -a ${FILE_LAST_LOG}
 
 
 # only run as root
 if [ "$(id -u)" != '0' ]; then
-   print_basename "This script has to be run as root" | tee -a ${LOGFILE}
+   print_basename "This script has to be run as root" 2>&1 | tee -a ${FILE_LAST_LOG}
    exit 1
 fi
 
 # check that the snapshot does not already exist
 if [ -e "/dev/${VOLGROUP}/${SNAPVOL}" ]; then
-   print_basename "LV snapshot already exists, please destroy it by hand first" | tee -a  ${LOGFILE}
+   print_basename "LV snapshot already exists, please destroy it by hand first" 2>&1 | tee -a  ${FILE_LAST_LOG}
    exit 1
 fi
 
 # create the lvm snapshot
 if ! /usr/sbin/lvcreate -L${SNAPSIZE} -s -n ${SNAPVOL} /dev/${VOLGROUP}/${ORIGVOL}  >/dev/null 2>&1; then
-   print_basename "Creating of the LV snapshot failed" 2>&1 >> ${LOGFILE}
+   print_basename "Creating of the LV snapshot failed" 2>&1 | tee -a ${FILE_LAST_LOG}
    exit 1
 fi
 
 # check that the mount point does not already exist, mount snapshot
 if ! [ -d ${MOUNTDIR}/${ORIGVOL} ]; then
-   print_basename "Creating mount point... ${MOUNTDIR}/${ORIGVOL}" | tee -a ${LOGFILE}
+   print_basename "Creating mount point... ${MOUNTDIR}/${ORIGVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
    mkdir -p ${MOUNTDIR}/${ORIGVOL}
 
    # mount snapshot
-   print_basename "Mounting LV snapshot... /dev/${VOLGROUP}/${SNAPVOL}" | tee -a ${LOGFILE}
+   print_basename "Mounting LV snapshot... /dev/${VOLGROUP}/${SNAPVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
    mount /dev/${VOLGROUP}/${SNAPVOL} ${MOUNTDIR}/${ORIGVOL}
    RES=$?
 
    if [ "$RES" != '0']; then
-      print_basename "Cannot mount LV snapshot: /dev/${VOLGROUP}/${SNAPVOL}" | tee -a ${LOGFILE}
+      print_basename "Cannot mount LV snapshot: /dev/${VOLGROUP}/${SNAPVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
       exit 1
    fi
 else
-   print_basename "Mount point exists: ${MOUNTDIR}/${ORIGVOL}" | tee -a ${LOGFILE}
-   print_basename "Mounting LV snapshot... /dev/${VOLGROUP}/${SNAPVOL}" | tee -a ${LOGFILE}
+   print_basename "Mount point exists: ${MOUNTDIR}/${ORIGVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
+   print_basename "Mounting LV snapshot... /dev/${VOLGROUP}/${SNAPVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
    mount /dev/${VOLGROUP}/${SNAPVOL} ${MOUNTDIR}/${ORIGVOL}
    RES=$?
 
    if [ "$RES" != '0' ]; then
-      print_basename "Cannot mount LV snapshot: /dev/${VOLGROUP}/${SNAPVOL}" | tee -a ${LOGFILE}
+      print_basename "Cannot mount LV snapshot: /dev/${VOLGROUP}/${SNAPVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
       exit 1
    fi
 fi
 
 # main command of the script that does the real stuff
-print_basename "Creating backup dir... ${BACKUPDIR}" | tee -a ${LOGFILE}
+print_basename "Creating backup dir... ${BACKUPDIR}" 2>&1 | tee -a ${FILE_LAST_LOG}
 mkdir -p ${BACKUPDIR}
 
 # create tar_exclude_var.txt
 if ! [ -f ${SCRIPTDIR}/tar_exclude_var.txt ]; then
-   print_basename "creating tar exclude file..." | tee -a ${LOGFILE}
+   print_basename "creating tar exclude file..." 2>&1 | tee -a ${FILE_LAST_LOG}
    touch ${SCRIPTDIR}/tar_exclude_var.txt
    echo "containerd" >> ${SCRIPTDIR}/tar_exclude_var.txt
    echo 'lost+found' >> ${SCRIPTDIR}/tar_exclude_var.txt
 fi
 
 if tar ${TAR_EXCLUDE_VAR} -cvzf ${BACKUPDIR}/${BACKUPNAME} ${MOUNTDIR}/${ORIGVOL}  >/dev/null 2>&1; then
-   print_basename "Created TAR archive: ${BACKUPDIR}/${BACKUPNAME}" | tee -a ${LOGFILE}
+   print_basename "Created TAR archive: ${BACKUPDIR}/${BACKUPNAME}" 2>&1 | tee -a ${FILE_LAST_LOG}
    # md5sum ${BACKUPDIR}/${BACKUPNAME} > ${BACKUPDIR}/${BACKUPNAME}.md5
    RES=0
 else
-   print_basename "Error: Create TAR archive failed." | tee -a ${LOGFILE}
+   print_basename "Error: Create TAR archive failed." 2>&1 | tee -a ${FILE_LAST_LOG}
    RES=1
 
 ##      exit (1);  # don't remove the snapshot just yet
@@ -169,40 +203,40 @@ fi
 
 if [ "$RES" != '1' ]; then    # prevent removal if error occurred above.
   # umount snapshot
-  print_basename "Unmounting LV snapshot..." | tee -a ${LOGFILE}
+  print_basename "Unmounting LV snapshot..." 2>&1 | tee -a ${FILE_LAST_LOG}
   umount ${MOUNTDIR}/${ORIGVOL}
 
   # remove snapshot
   if ! /usr/sbin/lvremove -f /dev/${VOLGROUP}/${SNAPVOL} >/dev/null 2>&1; then
-     print_basename "cannot remove the LV snapshot: /dev/${VOLGROUP}/${SNAPVOL}" | tee -a ${LOGFILE}
+     print_basename "cannot remove the LV snapshot: /dev/${VOLGROUP}/${SNAPVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
      RES=1
   else
-     print_basename "LV snapshot removed: /dev/${VOLGROUP}/${SNAPVOL}" | tee -a ${LOGFILE}
+     print_basename "LV snapshot removed: /dev/${VOLGROUP}/${SNAPVOL}" 2>&1 | tee -a ${FILE_LAST_LOG}
      RES=0
   fi
 fi
 
 # find old files and delete
-print_basename "Searching for old backups started..." | tee -a ${LOGFILE}
+print_basename "Searching for old backups started..." 2>&1 | tee -a ${FILE_LAST_LOG}
 COUNT=$(find ${SEARCHDIR} -type f -name "*.tgz" -mtime +${BACKUP_DAYS} |wc -l)
 
 if [ "${COUNT}" != 0 ]; then
-   print_basename "\"${COUNT}\" old backups will be deleted..." | tee -a ${LOGFILE}
-   find ${SEARCHDIR} -type f -name "*.tgz" -mtime +${BACKUP_DAYS} -delete >> ${LOGFILE}
-   #find ${SEARCHDIR} -type f -name "*.md5" -mtime +${BACKUP_DAYS} -delete >>  ${LOGFILE}
-   print_basename "Deleting empty folders..." | tee -a ${LOGFILE}
+   print_basename "\"${COUNT}\" old backups will be deleted..." 2>&1 | tee -a ${FILE_LAST_LOG}
+   find ${SEARCHDIR} -type f -name "*.tgz" -mtime +${BACKUP_DAYS} -delete >> ${FILE_LAST_LOG}
+   #find ${SEARCHDIR} -type f -name "*.md5" -mtime +${BACKUP_DAYS} -delete >>  ${FILE_LAST_LOG}
+   print_basename "Deleting empty folders..." 2>&1 | tee -a ${FILE_LAST_LOG}
    find ${SEARCHDIR} -empty -type d -delete
 else
-   print_basename "No old backups were found." | tee -a ${LOGFILE}
+   print_basename "No old backups were found." 2>&1 | tee -a ${FILE_LAST_LOG}
 fi
 
 # show all folders
-echo " "
-print_basename "======= Show all backup directories  ======="
+echo " " 2>&1 | tee -a ${FILE_LAST_LOG}
+print_basename "======= Show all backup directories  =======" 2>&1 | tee -a ${FILE_LAST_LOG}
 tree -i -d -L 1 ${SEARCHDIR} | sed '/director/d'
 
-print_basename "/------------ End of script ------------/" | tee -a ${LOGFILE}
-echo " " | tee -a ${LOGFILE}
+print_basename "/------------ End of script ------------/" 2>&1 | tee -a ${FILE_LAST_LOG}
+echo " " | tee -a ${FILE_LAST_LOG} 2>&1 | tee -a ${FILE_LAST_LOG}
 exit ${RES}
 
 # Examples without variables
